6598202e92675556391d5a316fc997acfb5b866a
Vincent Marchetti <vmarchetti@kshell.com>
2024-04-12 16:01:26 -0400

    refactoring with goal of separaring network fetch, parse to json, consruct Manifest instance

--- src/listeners.js
+++ src/listeners.js
@@ -1,25 +1,27 @@
-import { loadScene } from "./main.js";
+import { fetchManifestFromUrl , getManifestFromText } from "./main.js";
 
-// Non-3D listeners
 
 document
   .querySelector("button#load-manifest-from-url")
   .addEventListener("click", async () => {
     const manifestUrl = document.querySelector("input#manifest-url").value;
-    await loadScene(manifestUrl);
+    var manifest = await fetchManifestFromUrl(manifestUrl);
+    console.log("fetchManifestFromUrl result label " + manifest.getLabel() )
   });
 
 document
   .querySelector("button#load-manifest-from-text")
-  .addEventListener("click", async () => {
+  .addEventListener("click",  () => {
     const manifestText = document.querySelector("textarea#manifest-text").value;
-    await loadScene(manifestText);
+    var manifest = getManifestFromText(manifestText);
+    console.log("getManifestFromText result label " + manifest.getLabel().getValue() )
   });
 
 document
   .querySelector("select#manifest-select")
-  .addEventListener("change", (event) => {
+  .addEventListener("change", async (event) => {
     const manifestUrl = event.target.value;
     document.querySelector("input#manifest-url").value = manifestUrl;
-    loadScene(manifestUrl);
+    var manifest = await fetchManifestFromUrl(manifestUrl);
+    console.log("select then fetchManifestFromUrl result label " + manifest.getLabel().getValue("en") )
   });
--- src/main.js
+++ src/main.js
@@ -1,210 +1,69 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+const manifesto = require("manifesto.js/dist-commonjs/");
 
-import { IIIFManifest } from "./iiif";
+//let iiifManifest;
 
-const loader = new GLTFLoader();
-let renderer;
-let scene;
-let camera;
-let iiifManifest;
 
-// window.addEventListener("resize", onWindowResize, false);
-const firstManifestUrl = document.querySelector("select#manifest-select").value;
-document.querySelector("input#manifest-url").value = firstManifestUrl;
-loadScene(firstManifestUrl);
+//const firstManifestUrl = document.querySelector("select#manifest-select").value;
+//document.querySelector("input#manifest-url").value = firstManifestUrl;
+//loadSceneFromUrl(firstManifestUrl);
 
-export async function loadScene(manifestUrlOrJson) {
-  init();
-  await loadIIIFManifest(manifestUrlOrJson);
-  animate();
 
-  if (iiifManifest) {
-    document.querySelector("textarea#manifest-text").value = JSON.stringify(
-      iiifManifest.manifest.__jsonld,
-      undefined,
-      4,
-    );
-  }
-}
+/*
+    in the context of this webpage, will 
+    1. retrieve the json document pointed to by manifestUrl
+    2. does the JSON parsing into an object
+    3. stringifies the resulting object back into pretty text and
+       puts that text into the text area
+    4. constructs and returns the Manifest object 
+*/
 
-function init() {
-  const canvas = document.getElementById("canvas");
-  // remove any previous children
-  while (canvas.children.length > 0) {
-    canvas.removeChild(canvas.children[0]);
-  }
-
-  scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(
-    35,
-    canvas.clientWidth / canvas.clientHeight,
-    0.01,
-    100,
-  );
-  scene.add(camera);
-
-  const light = new THREE.AmbientLight(0x404040, 100); // soft white light
-  scene.add(light);
-
-  const light2 = new THREE.DirectionalLight("white", 0.5);
-  light.position.set(1, 1, 1);
-  scene.add(light2);
-
-  const grid = new THREE.GridHelper(20, 20);
-  scene.add(grid);
-
-  const axes = new THREE.AxesHelper(10);
-  axes.material.depthFunc = THREE.AlwaysDepth; // prevent z-fighting with grid, axes win
-  scene.add(axes);
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-  canvas.appendChild(renderer.domElement);
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-  camera.position.z = 20;
-  camera.position.y = 5;
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-}
-
-async function loadIIIFManifest(manifestUrlOrJson) {
-  iiifManifest = new IIIFManifest(manifestUrlOrJson);
-  await iiifManifest.loadManifest();
-
-  if (iiifManifest.scenes.length > 0) {
-    // Root scene
-    const manifestScene = iiifManifest.scenes[0];
-
-    // Add scene BG color
-    const bgColor = await manifestScene.getBackgroundColor();
-    if (
-      bgColor &&
-      "red" in bgColor &&
-      "green" in bgColor &&
-      "blue" in bgColor
-    ) {
-      scene.background = new THREE.Color(
-        `rgb(${bgColor.red}, ${bgColor.green}, ${bgColor.blue})`,
-      );
+/*
+    returns an object satisfying the IManifestOptions inteface
+    It is declared as a function with plan that in future development
+    it will be populated based in user input elements
+*/
+function getRequestedManifestOptions(){
+    var retVal = {
+        'defaultLabel' : "N/A",
+        'locale'       : "de"
     }
-
-    // Load individual model annotations
-    const annos = iiifManifest.annotationsFromScene(manifestScene);
-
-    const filteredAnnos = annos.filter((anno) => {
-      const body = anno.getBody3D();
-      return (
-        anno.getMotivation()?.[0] === "painting" &&
-        (body.isSpecificResource || body?.getType() === "model")
-      );
-    });
-
-    filteredAnnos.forEach((modelAnnotation) => {
-      loadModel(modelAnnotation);
-    });
-  }
+    return retVal;
 }
 
-function loadModel(modelAnnotation) {
-  let modelUrl;
-  if (modelAnnotation.getBody3D().isSpecificResource) {
-    modelUrl = modelAnnotation.getBody3D().getSource()?.id;
-  } else {
-    modelUrl = modelAnnotation.getBody3D().id;
-  }
-  const modelTarget = modelAnnotation.getTarget();
-
-  if (modelUrl && modelTarget) {
-    loader.load(modelUrl, (gltf) => {
-      let model = gltf.scene;
-
-      // Transform model
-      console.log(modelAnnotation.getBody3D());
-      if (modelAnnotation.getBody3D().isSpecificResource) {
-        const transforms = modelAnnotation.getBody3D().getTransform() || [];
-        console.log(transforms);
-        transforms.forEach((transform) => {
-          if (transform.isTransform) {
-            if (transform.isTranslateTransform) {
-              const translation = transform.getTranslation();
-              if (translation) {
-                console.log("translating");
-
-                model.position.set(translation.x, translation.y, translation.z);
-
-                let newModel = new THREE.Group();
-                newModel.attach(model);
-                model = newModel;
-              }
-            } else if (transform.isRotateTransform) {
-              // hi
-            } else if (transform.isScaleTransform) {
-              const scale = transform.getScale();
-              if (scale) {
-                console.log("scaling");
-                // figure out how to combine scaling with translation
-                model.scale.set(scale.x, scale.y, scale.z);
-
-                let newModel = new THREE.Group();
-                newModel.attach(model);
-                model = newModel;
-              }
-            }
-          }
-        });
-      }
-
-      // Position model within target scene if position selector present
-      if ( modelTarget.isSpecificResource ) {
-        const selector = modelTarget.getSelector();
-        if (selector && selector.isPointSelector) {
-          const position = selector.getLocation();
-          model.position.set(position.x, position.y, position.z);
-        }
-      }
-
-      scene.add(model);
-    });
-  }
-}
-
-function processTransform(model, transform) {
-  if (model && transform) {
-    model = doTransform(model, transform);
-    const newModel = new THREE.Group();
-    newModel.attach(model);
-    return newModel;
-  }
-}
-
-function doTransform(model, transform) {
-  if (transform.isTranslateTransform) {
-    const translation = transform.getTranslation();
-    if (translation) {
-      console.log("translating");
-      model.position.set(translation.x, translation.y, translation.z);
+export async function fetchManifestFromUrl(manifestUrl) {
+  
+    var response = await fetch(manifestUrl);
+    if (response.ok){
+        var manifestDoc = await response.text();
     }
-  } else if (transform.isRotateTransform) {
-    return rotate;
-  } else if (transform.isScaleTransform) {
-    const scale = transform.getScale();
-    if (scale) {
-      console.log("scaling");
-      model.scale.set(scale.x, scale.y, scale.z);
+    else{
+        throw new Error("network fetch of manifest failed");
     }
-  }
+    
+    try{
+        var manifestObj = JSON.parse( manifestDoc);
+    }
+    catch(exc)
+    {
+        console.log("err " + exc);
+        console.log(manifestDoc);
+    }
+    var options = getRequestedManifestOptions();
+    var retVal = new manifesto.Manifest(manifestObj, options)
+    console.log("manifest parsed into Manifesto object");
+    // prepare the pretty text
+    // reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+    var prettyText = JSON.stringify( manifestObj, undefined, 4);
+    document.querySelector("textarea#manifest-text").value = prettyText;
+
+    return retVal;
 }
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+
+export function getManifestFromText(manifestDoc) {
+    var manifestObj = JSON.parse( manifestDoc);
+    var options = getRequestedManifestOptions();
+    return new manifesto.Manifest(manifestObj, options)
 }
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
+
