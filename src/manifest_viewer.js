116587519d06acde552d0dfc0a86d4305b258125
Vincent Marchetti <vmarchetti@kshell.com>
2024-04-15 12:44:48 -0400

    implementation of manifestViewer

---
+++ src/manifest_viewer.js
@@ -0,0 +1,71 @@
+
+
+/* 
+script level: define structure and event handlers to determine when
+both the window load event has been fired and the x3dom ready event
+ */
+
+
+
+let _pre_init_state = {   
+    x3dom:false, 
+    document:false
+};
+
+
+x3dom.runtime.ready = function(element) {
+    console.log("x3dom.ready fired");
+    _pre_init_state.x3dom = true;
+    if ( _pre_init_state.x3dom && _pre_init_state.document ){
+        initialize_viewer();
+    } 
+};
+
+window.addEventListener("load" , (event) => {
+    console.log("document load fired");
+    _pre_init_state.document = true;
+    if ( _pre_init_state.x3dom && _pre_init_state.document ) initialize_viewer();
+});
+
+let manifestViewer = null;
+
+
+initialize_viewer = function () {
+    console.log("initialize_viewer called");
+    
+    manifestViewer = {
+        triad_switch_node : document.getElementById("triad-switch-node"),
+        axes_description  : document.getElementById("axes-description"),
+        setAxesVisible( value ){
+            // value is a boolean
+            console.log("set isAxesVisible to " + value);
+            var newChoice = (value)? 0 : -1;
+            this.triad_switch_node.setAttribute("whichChoice", newChoice );
+            this.axes_description.style.display =(value)?"block":"none";
+        },
+        
+        navigationinfo_node   : document.getElementById("navigationinfo-node"),
+        headlight_description : document.getElementById("headlight-description"),
+        setHeadlightOn( value ){
+            // value is a boolean
+            console.log("setHeadlightOn to " + value);
+            var newBool = (value)?"true":"false";
+            this.navigationinfo_node.setAttribute("headlight", newBool);
+            this.headlight_description.style.display =(value)?"block":"none";
+        }
+    };
+    
+    let show_axes_checkbox = document.getElementById("show-axes-checkbox");
+    show_axes_checkbox.checked = true;
+    show_axes_checkbox.addEventListener("click", (event) => 
+    {
+        manifestViewer.setAxesVisible(  show_axes_checkbox?.checked);
+    });
+    
+    let headlight_on_checkbox = document.getElementById("headlight-on-checkbox");
+    headlight_on_checkbox.checked = true;
+    headlight_on_checkbox.addEventListener("click", (event) => 
+    {
+        manifestViewer.setHeadlightOn(  headlight_on_checkbox?.checked);
+    });
+};
