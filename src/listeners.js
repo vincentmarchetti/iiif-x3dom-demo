f7af95e0f86297c86015335f7ce1170f45a9b5cb
Vincent Marchetti <vmarchetti@kshell.com>
2025-05-27 22:09:09 -0400

    Simplified UI for loading directly from url in search field

--- src/listeners.js
+++ src/listeners.js
@@ -2,23 +2,9 @@ import { fetchManifestFromUrl , getManifestFromText } from "./main.js";
 
 
 
-document
-  .querySelector("button#load-manifest-from-text")
-  .addEventListener("click",  () => {
-    const manifestText = document.querySelector("textarea#manifest-text").value;
-    var manifest = getManifestFromText(manifestText);
-    fireNewManifestEvent( manifest );
-    //console.log("getManifestFromText result label " + manifest.getLabel().getValue() )
-  });
 
  
 
-document.addEventListener("load_text_from_url", async (event) =>
-{
-    var manifestUrl = event.detail.url;
-    console.log("in load_text_from_url " + manifestUrl);
-    await fetchManifestFromUrl(manifestUrl);
-});
 
 function fireNewManifestEvent( newManifest ){
     console.log("firing new_manifest");
